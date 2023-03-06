af623db4a6cd254398d5711d386d6496dc5423d2
Shrikant <125242220+shrikantjawla661@users.noreply.github.com>
2023-03-06 23:01:25 +0530

    updated category pages

--- new_ciMoney/admin/utils/assets/js/categories/addNewCategories.js
+++ new_ciMoney/admin/utils/assets/js/categories/addNewCategories.js
@@ -1,72 +1,74 @@
 function clearPreview() {
-    let preview = document.getElementById(`img-preview`);
-    preview.src = '';
-  }
-  
-  
-  function showPreview(event) {
-    event.preventDefault()
-    if (event.target.files.length > 0) {
-        let src = URL.createObjectURL(event.target.files[0])
-        var preview = document.getElementById(`${event.target.id}-preview`)
-        preview.src = src
-        preview.style.display = "block"
-    }
-  }
-  
-  function resetFeilds(){
-    document.querySelector('#name').value = '';
-    document.querySelector('#desc').value = '';
-    document.querySelector('#sequence').value = '';
-    $("#img").val('');
-    clearPreview();
-  }
+  let preview = document.getElementById(`img-preview`);
+  preview.src = '';
+  $("#img").val('');
+}
 
 
-  function addAOffer() {
-    const name = document.querySelector("#name").value;
-    const desc = document.querySelector("#desc").value;
-    const sequence = document.querySelector("#sequence").value;
-    const status = document.querySelector("#status").value;
-    const img = document.querySelector("#img").files[0];
-    const offers = $('#multiSelectWrapper').children().children('input');
-    let offerIds=[];
-    for(let i of offers){
-      if(i.checked) offerIds.push(i.value)
-    }
-  
-    if (
-      name === "" ||
-      desc === "" ||
-      sequence === "" ||
-      status === "" ||
-      img === ""
-    ) {
-      alert("Please fill all the fields!");
-    }
-  
-    let form = new FormData();
-  
-    form.append("name", name);
-    form.append("desc", desc);
-    form.append("sequence", sequence);
-    form.append("status", status);
-    form.append("img", img);
-    form.append('offers_array',offerIds);
-  
-    fetch("/admin/categories/addNewCategory", {
-      method: "POST",
-      body: form,
+function showPreview(event) {
+  event.preventDefault()
+  if (event.target.files.length > 0) {
+    let src = URL.createObjectURL(event.target.files[0])
+    var preview = document.getElementById(`${event.target.id}-preview`)
+    preview.src = src
+    preview.style.display = "block"
+  }
+}
+
+function resetFeilds() {
+  document.querySelector('#name').value = '';
+  document.querySelector('#desc').value = '';
+  document.querySelector('#sequence').value = '';
+  $("#img").val('');
+  clearPreview();
+}
+$(document).ready(() => {
+  $('#mselect').chosen()
+  // $('#mselect').val(['24', '23', '22']).trigger('chosen:updated')
+})
+function addAOffer() {
+  const name = document.querySelector("#name").value;
+  const desc = document.querySelector("#desc").value;
+  const sequence = document.querySelector("#sequence").value;
+  const status = document.querySelector("#status").value;
+  const img = document.querySelector("#img").files[0];
+  let offerIds = [];
+  $('#mselect').change(() => {
+    offerIds = $('#mselect').val();
+  })
+  if (
+    name === "" ||
+    desc === "" ||
+    sequence === "" ||
+    status === "" ||
+    img === ""
+  ) {
+    alert("Please fill all the fields!");
+  }
+
+  let form = new FormData();
+
+  form.append("name", name);
+  form.append("desc", desc);
+  form.append("sequence", sequence);
+  form.append("status", status);
+  form.append("img", img);
+  form.append('offers_array', offerIds);
+
+  fetch("/admin/categories/addNewCategory", {
+    method: "POST",
+    body: form,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      // console.log(res);
+      resetFeilds()
+      alert('Entry has been added successfully!')
     })
-      .then((res) => res.json())
-      .then((res) => {
-        // console.log(res);
-        resetFeilds()
-        alert('Entry has been added successfully!')
-      })
-      .catch((err) => {
-        console.log(err);
-        alert(err.message)
-      });
-  }
-  
+    .catch((err) => {
+      console.log(err);
+      alert(err.message)
+    });
+}
+
+
